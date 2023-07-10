f114e3f51cd05802734d0a76540bc12f0643e2a3
DanielHalfeld <104243301+DanielHalfeld@users.noreply.github.com>
2023-07-09 23:18:45 -0300

    pronto

---
+++ JAVASCRIPT/dicas.js
@@ -0,0 +1,18 @@
+/*
+Operadores relacionais:
+
+== igual
+!= diferente
+> maior
+< menor
+>= maior ou igual
+<= menor ou igual
+
+Operadores lógicos:
+
+&& e
+|| ou
+
+*/
+let resultado = 2==3 && 1==3
+console.log(resultado)
