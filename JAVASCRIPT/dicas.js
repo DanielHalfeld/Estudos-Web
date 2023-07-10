8dfadbfd7d6be241f9a3370e7f5f15b0ea26396a
DanielHalfeld <104243301+DanielHalfeld@users.noreply.github.com>
2023-07-09 23:30:28 -0300

    pronto

--- JAVASCRIPT/dicas.js
+++ JAVASCRIPT/dicas.js
@@ -13,6 +13,20 @@ Operadores lógicos:
 && e
 || ou
 
+Estruturas condicionais if else:
+
+if(){
+
+}else{}
+
+----------------
+
+if(){
+
+}
+else if(){
+
+}
 */
 let resultado = 2==3 && 1==3
 console.log(resultado)
