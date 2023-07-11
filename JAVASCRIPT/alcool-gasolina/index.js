6f00325b53b80ea81d4e607da4f62fb6dfc13b60
DanielHalfeld <104243301+DanielHalfeld@users.noreply.github.com>
2023-07-10 22:37:51 -0300

    Add files via upload

---
+++ JAVASCRIPT/alcool-gasolina/index.js
@@ -0,0 +1,15 @@
+function calcularMelhorPreco() {
+
+    let precoAlcool = document.getElementById("alcool").value
+    let precoGasolina = document.getElementById("gasolina").value;    
+
+    if( precoAlcool != "" ){
+
+    }else{
+        alert("Preencha o preço do Álcool!")
+    }
+
+}
+        
+
+
