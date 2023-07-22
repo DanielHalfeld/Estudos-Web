4c434639368b8ead7ef4e701ac4bd3d24bab1fc8
DanielHalfeld <104243301+DanielHalfeld@users.noreply.github.com>
2023-07-22 23:37:02 +0000

    a

--- C#/HelloTests/UnitTest1.cs
+++ C#/HelloTests/UnitTest1.cs
@@ -1,10 +1,17 @@
-namespace HelloTests;
+using Xunit;
+
+namespace MyFirstUnitTest;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void PassingTest()
     {
 
+        Assert.Equal(4, Add(2, 2));
+
+    }
+    int Add(int x, int y){
+        return x + y;
     }
 }
